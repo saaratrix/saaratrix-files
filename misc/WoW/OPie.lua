c802aa48f5a3d2d26ba145911075f20c89a07650
saaratrix <saaratrix@gmail.com>
2025-03-23 15:06:37 +0200

    Added OPie config too.

---
+++ misc/WoW/OPie.lua
@@ -0,0 +1,420 @@
+
+OneRing_Config = nil
+OPie_SavedData = {
+["CharProfiles"] = {
+},
+["_GameVersion"] = "11.1.0",
+["_OPieVersion"] = "Ælling 4 (4.128)",
+["ProfileStorage"] = {
+["default"] = {
+["RotationTokens"] = {
+["ABueE4bvwau"] = "ABueE4bvway",
+["ABueE4bvwac"] = "ABueE4bvwa5",
+["ABueE4bvwaA"] = "ABueE4bvwaT",
+["ABueE4bvwam"] = "ABueE4bvwan",
+["ABueE4bvwa3"] = "ABueE4bvwas",
+["ABueE4bvwaP"] = "ABueE4bvwaJ",
+["ABueE4bvwa6"] = "ABueE4bvwav",
+["ABueE4bvwak"] = "ABueE4bvwaf",
+["ABueE4bvwaZ"] = "ABueE4bvwa9",
+},
+["RingOptions"] = {
+["Portals#InteractionMode"] = 1,
+["Portals#GhostMIRings"] = true,
+},
+["Bindings"] = {
+["Useful"] = "K",
+["SpecMenu"] = "CTRL-N",
+["CommonTrades"] = false,
+["Portals"] = "SHIFT-L",
+["RaidSymbols"] = false,
+["OPieAutoQuest"] = false,
+["DKCombat"] = false,
+["UI"] = "J",
+["WorldMarkers"] = "ALT-R",
+["Useful2"] = "SHIFT-K",
+["OPieTracking"] = false,
+},
+},
+},
+["PersistentStorage"] = {
+["RingKeeper"] = {
+["sWarWithin"] = {
+{
+["sliceToken"] = "ABueE4bvwa9",
+["id"] = 445269,
+},
+{
+["sliceToken"] = "ABueE4bvwaS",
+["id"] = 445414,
+},
+{
+["id"] = 445441,
+["sliceToken"] = "ABueE4bvwaD",
+},
+{
+["id"] = 445440,
+["sliceToken"] = "ABueE4bvwaF",
+},
+{
+["id"] = 445444,
+["sliceToken"] = "ABueE4bvwaG",
+},
+{
+["id"] = 445443,
+["sliceToken"] = "ABueE4bvwa0",
+},
+{
+["id"] = 1216786,
+["sliceToken"] = "ABueE4bvwaH",
+},
+{
+["sliceToken"] = "ABueE4bvwaK",
+["id"] = 445416,
+},
+{
+["sliceToken"] = "ABueE4bvwaL",
+["id"] = 445417,
+},
+["name"] = "Portals - War Within",
+["save"] = true,
+},
+["Useful"] = {
+{
+"toy",
+198173,
+["sliceToken"] = "ABueQ9pAA8w",
+},
+{
+"toy",
+156833,
+["sliceToken"] = "ABueQ9pAA8e",
+},
+{
+"toy",
+217724,
+["sliceToken"] = "ABueQ9pAA8r",
+},
+{
+"toy",
+220777,
+["sliceToken"] = "ABueQ9pAA81",
+},
+{
+["sliceToken"] = "ABueQ9pAA8t",
+["id"] = 436854,
+},
+{
+"toy",
+140192,
+["sliceToken"] = "ABueQ9pAA8y",
+},
+{
+"toy",
+110560,
+["sliceToken"] = "ABueQ9pAA8u",
+},
+{
+"toy",
+219387,
+["sliceToken"] = "ABueQ9pAA8i",
+},
+{
+"toy",
+183716,
+["sliceToken"] = "ABueQ9pAA8o",
+},
+["offset"] = 0,
+["save"] = true,
+["name"] = "Useful",
+},
+["lsPandaria"] = {
+{
+["sliceToken"] = "ABueE4bvwai",
+["id"] = 131204,
+},
+{
+["sliceToken"] = "ABueE4bvwa2",
+["id"] = 131205,
+},
+{
+["sliceToken"] = "ABueE4bvwao",
+["id"] = 131206,
+},
+{
+["sliceToken"] = "ABueE4bvwap",
+["id"] = 131222,
+},
+{
+["sliceToken"] = "ABueE4bvwaa",
+["id"] = 131225,
+},
+{
+["sliceToken"] = "ABueE4bvwas",
+["id"] = 131228,
+},
+["name"] = "Portals - Pandaria",
+["save"] = true,
+},
+["hadowlands"] = {
+{
+["id"] = 354468,
+["sliceToken"] = "ABueE4bvwaQ",
+},
+{
+["id"] = 354466,
+["sliceToken"] = "ABueE4bvwaW",
+},
+{
+["id"] = 354465,
+["sliceToken"] = "ABueE4bvwaE",
+},
+{
+["id"] = 354463,
+["sliceToken"] = "ABueE4bvwa7",
+},
+{
+["sliceToken"] = "ABueE4bvwaT",
+["id"] = 354464,
+},
+{
+["sliceToken"] = "ABueE4bvwaY",
+["id"] = 354462,
+},
+["name"] = "Portals - Shadowlands",
+["save"] = true,
+},
+["talsLegion"] = {
+{
+["sliceToken"] = "ABueE4bvwan",
+["id"] = 393766,
+},
+["name"] = "Portals - Legion",
+["save"] = true,
+},
+["alsClassic"] = {
+{
+["sliceToken"] = "ABueE4bvwal",
+["id"] = 131231,
+},
+{
+["sliceToken"] = "ABueE4bvwaz",
+["id"] = 131229,
+},
+{
+["sliceToken"] = "ABueE4bvwa5",
+["id"] = 131232,
+},
+{
+["sliceToken"] = "ABueE4bvwax",
+["id"] = 159902,
+},
+["name"] = "Portals - Classic",
+["save"] = true,
+},
+["Portals"] = {
+{
+"ring",
+"alsClassic",
+["sliceToken"] = "ABueE4bvwac",
+},
+{
+"ring",
+"ingCrusade",
+["sliceToken"] = "ABueE4bvwa6",
+},
+{
+"ring",
+"sCataclysm",
+["sliceToken"] = "ABueE4bvwau",
+},
+{
+"ring",
+"lsPandaria",
+["sliceToken"] = "ABueE4bvwa3",
+},
+{
+"ring",
+"alsDraenor",
+["sliceToken"] = "ABueE4bvwak",
+},
+{
+"ring",
+"talsLegion",
+["sliceToken"] = "ABueE4bvwam",
+},
+{
+"ring",
+"PortalsBFA",
+["sliceToken"] = "ABueE4bvwaP",
+},
+{
+"ring",
+"hadowlands",
+["sliceToken"] = "ABueE4bvwaA",
+},
+{
+"ring",
+"sWarWithin",
+["sliceToken"] = "ABueE4bvwaZ",
+},
+["name"] = "Portals",
+["embed"] = true,
+["save"] = true,
+},
+["OPieFlagStore"] = {
+["StoreVersion"] = 3,
+},
+["sCataclysm"] = {
+{
+["sliceToken"] = "ABueE4bvway",
+["id"] = 445424,
+},
+["name"] = "Portals - Cataclysm",
+["save"] = true,
+},
+["alsDraenor"] = {
+{
+["sliceToken"] = "ABueE4bvwad",
+["id"] = 159895,
+},
+{
+["sliceToken"] = "ABueE4bvwaf",
+["id"] = 159898,
+},
+{
+["sliceToken"] = "ABueE4bvwag",
+["id"] = 159901,
+},
+{
+["sliceToken"] = "ABueE4bvwah",
+["id"] = 159899,
+},
+{
+["sliceToken"] = "ABueE4bvwa4",
+["id"] = 159900,
+},
+{
+["sliceToken"] = "ABueE4bvwaj",
+["id"] = 159896,
+},
+["name"] = "Portals - Draenor",
+["save"] = true,
+},
+["PortalsBFA"] = {
+{
+["id"] = 373274,
+["sliceToken"] = "ABueE4bvwaU",
+},
+{
+["id"] = 467553,
+["sliceToken"] = "ABueE4bvwa8",
+},
+{
+["id"] = 410071,
+["sliceToken"] = "ABueE4bvwaI",
+},
+{
+["id"] = 424187,
+["sliceToken"] = "ABueE4bvwaO",
+},
+{
+["sliceToken"] = "ABueE4bvwaJ",
+["id"] = 445418,
+},
+["name"] = "Portals - Battle for Azeroth",
+["save"] = true,
+},
+["UI"] = {
+{
+"uipanel",
+"guild",
+["sliceToken"] = "ABueQ9yin4y",
+},
+{
+"uipanel",
+"achievements",
+["sliceToken"] = "ABueQ9yin4u",
+},
+{
+"uipanel",
+"groupfinder",
+["sliceToken"] = "ABueQ9yin4i",
+},
+{
+"uipanel",
+"adventureguide",
+["sliceToken"] = "ABueQ9yin42",
+},
+{
+"uipanel",
+"collections",
+["sliceToken"] = "ABueQ9yin4o",
+},
+{
+"uipanel",
+"profs",
+["sliceToken"] = "ABueQ9yin4p",
+},
+{
+"uipanel",
+"calendar",
+["sliceToken"] = "ABueQ9yin4a",
+},
+{
+"uipanel",
+"reputation",
+["sliceToken"] = "ABueQGfPLGw",
+},
+["name"] = "UI",
+["save"] = true,
+},
+["agonflight"] = {
+["name"] = "Portals - Dragonflight",
+["save"] = true,
+},
+["Useful2"] = {
+{
+"toy",
+230850,
+["sliceToken"] = "ABueE4bvwaw",
+},
+{
+"mount",
+2296,
+["sliceToken"] = "ABueE4bvwae",
+},
+{
+"mount",
+1744,
+["sliceToken"] = "ABueE4bvwar",
+},
+{
+"toy",
+236687,
+["sliceToken"] = "ABueE4bvwa1",
+},
+{
+"toy",
+142542,
+["sliceToken"] = "ABueE4bvwat",
+},
+["name"] = "Useful 2",
+["save"] = true,
+},
+["ingCrusade"] = {
+{
+["sliceToken"] = "ABueE4bvwav",
+["id"] = 159897,
+},
+{
+["id"] = 373262,
+["sliceToken"] = "ABueE4bvwab",
+},
+["name"] = "Portals - Burning Crusade",
+["save"] = true,
+},
+},
+},
+["_GameLocale"] = "enUS",
+}
